546283e96a75db7764bb710ed1abc8d618b341e1
Priyankakhedkar10 <priyankakhedkar.c21@gmail.com>
2026-09-08 17:10:03 +0530

    product card added

--- src/utils/products.js
+++ src/utils/products.js
@@ -46,6 +46,54 @@ const products = [
     price: 999,
     image: "/images/perfume.jpg",
     description: "Refreshing floral fragrance for everyday use."
+  },
+   {
+    id: 7,
+    name: "Matte Lipstick",
+    category: "Makeup",
+    price: 399,
+    image: "/images/lipstick1.jpg",
+    description: "Long-lasting matte lipstick with beautiful shades."
+  },
+  {
+    id: 8,
+    name: "Liquid Foundation",
+    category: "Makeup",
+    price: 499,
+    image: "/images/foundation1.jpg",
+    description: "Smooth and lightweight foundation for natural coverage."
+  },
+  {
+    id: 9,
+    name: "Nail Paint",
+    category: "Nails",
+    price: 299,
+    image: "/images/nailpaint1.jpg",
+    description: "Beautiful nail color with long-lasting shine."
+  },
+  {
+    id: 10,
+    name: "Mascara",
+    category: "Makeup",
+    price: 299,
+    image: "/images/mascara1.jpg",
+    description: "Volumizing mascara for bold and beautiful lashes."
+  },
+  {
+    id: 11,
+    name: "Face Moisturizer",
+    category: "Skincare",
+    price: 799,
+    image: "/images/moisturizer1.jpg",
+    description: "Hydrating moisturizer for soft and glowing skin."
+  },
+  {
+    id: 12,
+    name: "Floral Perfume",
+    category: "Fragrance",
+    price: 799,
+    image: "/images/perfume1.jpg",
+    description: "Refreshing floral fragrance for everyday use."
   }
 ];
 export default products;
